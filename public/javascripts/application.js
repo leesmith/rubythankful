3a45e6d78788daf1c10fa6abeb882eb7e74877c7
Michael Bleigh <michael@intridea.com>
2011-08-15 11:05:30 -0300

    Compiling last.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,7 +3,7 @@
     return $.getJSON("http://search.twitter.com/search.json?q=%23rubythankful&rpp=100&callback=?", function(search) {
       return $.each(search.results, function() {
         var linked_text;
-        linked_text = this.text.replace(/@([0-9a-z_]+)/g, "<a href='http://twitter.com/$1'>@$1</a>").replace(/#([0-9a-z_-]+)/g, "<a href='http://twitter.com/search?q=%23$1'>#$1</a>");
+        linked_text = this.text.replace(/@([0-9a-z_]+)/gi, "<a href='http://twitter.com/$1'>@$1</a>").replace(/#([0-9a-z_-]+)/gi, "<a href='http://twitter.com/search?q=%23$1'>#$1</a>");
         return $('#stream').append("<article class='tweet'>\n  <a class='avatar' href='http://twitter.com/" + this.from_user + "'><img src='" + this.profile_image_url + "' alt='" + this.from_user + "'/></a>\n  <div class='body'>\n    <a class='author' href='http://twitter.com/" + this.from_user + "'>" + this.from_user + "</a>\n    <span class='text'>" + linked_text + "</span>\n  </div>\n</article>");
       });
     });
